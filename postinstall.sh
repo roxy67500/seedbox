a8b4d495af621d8bf3cef4928e1466ee0d4a290e
roxy67500 <142294925+roxy67500@users.noreply.github.com>
2023-08-19 10:45:32 +0200

    Create postinstall.sh

---
+++ postinstall.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Version 1.0
+
+# This script is automatically executed by the preseed file
+
+# Permit Root login:
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+
+# Add Docker’s official GPG key:
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Installing docker and samba for netbios resolution
+apt update -y
+apt install -y docker-ce docker-ce-cli containerd.io docker-compose samba
+
+# Download startup script and make it executable
+wget -O /root/startup.sh https://raw.githubusercontent.com/roxy67500/seedbox/main/startup.sh
+chmod +x /root/startup.sh
+
+# Add script to be executed on boot then removed once ran
+echo "#!/bin/sh -e \n/root/startup.sh \nrm -f /etc/rc.local \nexit 0" > /etc/rc.local
+chmod 755 /etc/rc.local
